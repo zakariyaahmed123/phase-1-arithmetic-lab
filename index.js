289e6e18508ec94effc541176fc0d4400d9c66c8
alimajid <alidzk98@gmail.com>
2022-10-11 13:49:49 +0300

    Done !

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+
+let num1 = 2,
+  num2 = 31,
+  num3 = 2,
+  num4 = 8;
+
+let multiply = num1 * num2;
+let random = Math.floor(Math.random()) + 1;
+let mod = num4 / num3;
+let max = Math.max(1, 8, 6, 11, 20);
+// console.log(result);
